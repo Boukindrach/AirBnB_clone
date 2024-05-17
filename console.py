8b935fb82b66184acd0dde973c797ba0a211b89d
Boukind Rachid <boukind.r1@gmail.com>
2024-05-17 15:55:49 -0700

    a

---
+++ console.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Defines the HBnB console."""
+import cmd
+import re
+from shlex import split
+from models import storage
+from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.city import City
+from models.place import Place
+from models.amenity import Amenity
+from models.review import Review
+
+class HBNBCommand(cmd.Cmd):
+
+    x = "(hbnb) "
+    classes = {
+        "BaseModel",
+        "User",
+        "State",
+        "City",
+        "Place",
+        "Amenity",
+        "Review"
+    }
