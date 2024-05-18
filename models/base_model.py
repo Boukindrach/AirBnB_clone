384f4b3e2dc691b46c1edf6a055308fce1d79516
Boukind Rachid <boukind.r1@gmail.com>
2024-05-18 15:03:58 -0700

    a

--- models/base_model.py
+++ models/base_model.py
@@ -9,12 +9,19 @@ class BaseModel:
         self.id = str(uuid.uuid4())
         self.created_at = datetime.today()
         self.updated_at = datetime.today()
+        tform = "%Y-%m-%dT%H:%M:%S.%f"
+        if len(kwargs) != 0:
+            for k, v in kwargs.items():
+                if k == "created_at" or k == "updated_at":
+                    self.__dict__[k] = datetime.strptime(v, tform)
+                else:
+                    self.__dict__[k] = v
+        else:
+            models.storage.new(self)
 
-   def  save(self):
+    def  save(self):
        self.update_at = datetime.now()
-
-
-
+       models.storage.save()
 
     def __str__(self):
-        return "[BaseModel] ({}) {}".format(self.id, self.__dict__)
+        return "[] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
